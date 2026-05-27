22394e223ec7f044a448d2ee76c493fc93bb9f99
Edwin <edwinmg27@gmail.com>
2026-05-27 17:25:23 -0400

    Agregar implementación del ejercicio 4: modularizar calculadora en un nuevo archivo

---
+++ 8.Funciones_como_bloques_de_logica/ejercicio4_modularizar_calculador.dart
@@ -0,0 +1,11 @@
+void main() {
+  //Debemos modularizar una calculadora, como sabemos modularizar significa dividir un problema en pequenas partes para lograr su solucion.
+  /**
+  * Como podemos realizar esta modularizacion? mentalmente he pensado en obtener 2 numeros por input y tambien por input el tipo de operacion
+  esos 3 inputs pasarlos a una condicional switch y la condicion que va a evaluar el switch sera el operador y los cases que hagan match son los que tendran el operador y dentro de los cases se realiza el calculo llamando a cada funcion que sea necesaria
+  definiciones de funciones, se van a definir cada funcion por cada operacion para mantener la regla de responsabilidad unica.
+  la funcion va a recibir 2 parametros que son los numeros
+  dentro del cuerpo de la funcion se realizara el calculo de la operacion
+  y los argumentos se enviaran dentro de los cases al invocar la funcion
+  */
+}
